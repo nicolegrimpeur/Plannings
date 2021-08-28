ce92a71733cada0a7c67d1c28e79c77ed44e9ffa
Nicolas Barrat <nicolas.barrat@student.junia.com>
2021-08-28 21:32:20 +0200

    Ajout système de log

--- api/script.js
+++ api/script.js
@@ -1,3 +1,5 @@
+// noinspection JSCheckFunctionSignatures
+
 const fs = require('fs');
 
 const Gestion = require('./gestion');
@@ -10,4 +12,32 @@ const paths = [
   {residence: 'sto', id: 'machine2'},
 ];
 
-for (let path of paths) gestion.remiseZero(path.id, path.residence);
+// on récupère le fichier de log
+const log = JSON.parse(fs.readFileSync('./log/log.json'));
+
+// on récupère la date actuelle
+const time = new Date(Date());
+
+// options pour l'enregistrement de la date
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' };
+
+try {
+  for (let path of paths) gestion.remiseZero(path.id, path.residence);
+
+  log.log.push({
+    dateModif: time.toLocaleDateString('fr-FR', options),
+    modification: 'succès'
+  });
+
+}
+catch (err) {
+  log.log.push({
+    dateModif: time.toLocaleDateString('fr-FR', options),
+    modification: 'erreur',
+    erreur: err
+  });
+
+}
+
+// on enregistre les modifications
+fs.writeFileSync('./log/log.json', JSON.stringify(log, null, 2));
