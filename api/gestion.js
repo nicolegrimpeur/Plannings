5f2567a4793ece0a3d83e4f2d27f7acfac9e796f
Nicolas Barrat <nicolas.barrat@student.junia.com>
2021-08-28 21:11:20 +0200

    Ajout de commentaires

--- api/gestion.js
+++ api/gestion.js
@@ -1,20 +1,25 @@
+// noinspection JSCheckFunctionSignatures
+
 const fs = require("fs");
 module.exports = class Gestion {
   fs = '';
   constructor() {
   }
 
+  // initialise le système fs
   addFS(fs_) {
     this.fs = fs_;
   }
 
   initFile(id, residence, res) {
+    // informations nécessaires pour l'inscription
     const data = {
       "nom": "",
       "prenom": "",
       "chambre": ""
     }
 
+    // tous les créneaux possibles
     const horaires = {
       '7H': data,
       '8H30': data,
@@ -27,6 +32,7 @@ module.exports = class Gestion {
       '19H': data,
       '20H30': data};
 
+    // tous les jours de la semaine
     const jours = {
       'dimanche1': horaires,
       'lundi': horaires,
@@ -38,72 +44,101 @@ module.exports = class Gestion {
       'dimanche2': horaires
     };
 
+    // on écrit le fichier de planning avec ces données de base
     fs.writeFileSync('./plannings/' + residence + '/' + id + '.json', JSON.stringify(jours, null, 2));
 
+    // on initialise le système d'historique correspondant à ce planning
     this.initHistorique(id, residence);
 
+    // si la fonction a été appelé via requête http
     if (res !== undefined) res.status(200).json('ok');
   }
 
+  // ajoute un créneau
   addCreneau(informations, res) {
     let id, nom, prenom, residence, chambre, jour, heure;
+
+    // on récupère les informations dans la requête
     [id, residence, jour, heure, nom, prenom, chambre] = informations.split('+');
 
     // on vérifie que l'on a toutes les infos nécessaires
     if (id !== undefined && residence !== undefined && jour !== undefined && heure !== undefined && nom !== undefined && prenom !== undefined && chambre !== undefined) {
+      // si le try passe, le fichier existe, sinon on le créé
       try {
+        // on récupère le planning
         const planning = JSON.parse(fs.readFileSync('./plannings/' + residence + '/' + id + '.json'));
 
         // on vérifie qu'il n'y a pas déjà quelqu'un sur ce créneau
         if (planning[jour][heure].nom === '') {
+          // on inscrit la personne
           planning[jour][heure].nom = nom;
           planning[jour][heure].prenom = prenom;
           planning[jour][heure].chambre = chambre;
+
+          // on enregistre l'inscription
           this.fs.writeFileSync('./plannings/' + residence + '/' + id + '.json', JSON.stringify(planning, null, 2));
 
+          // on ajoute l'inscription à l'historique
           this.addHistorique('Inscription sur le créneau', id, residence, nom, prenom, chambre, jour, heure);
 
+          // on renvoi une réponse positive à l'inscription
           res.status(200).send('Inscription réussi');
         }
         else {
+          // quelqu'un est déjà inscris, on envoi une réponse d'erreur
           res.status(404).send('Créneau déjà utilisé')
         }
       }
       catch (err) {
+        // on initialise le fichier de planning
         this.initFile(residence, id);
+        // on refait l'inscription
         this.addCreneau(informations, res);
       }
     }
     else {
+      // il manque des informations, donc on renvoit une erreur
       res.status(404).send('Erreur dans les informations fournies');
     }
   }
 
+  // suppression d'un créneau déjà existant
   removeCreneau(informations, res) {
     let id, nom, prenom, residence, chambre, jour, heure;
+
+    // on récupère les informations dans la requête
     [id, residence, jour, heure, nom, prenom, chambre] = informations.split('+');
 
     // on vérifie que l'on a toutes les infos nécessaires
     if (id !== undefined && residence !== undefined && jour !== undefined && heure !== undefined && nom !== undefined && prenom !== undefined && chambre !== undefined) {
+      // si le try passe, le fichier existe, sinon on renvoi une erreur
       try {
+        // on récupère le planning
         const planning = JSON.parse(fs.readFileSync('./plannings/' + residence + '/' + id + '.json'));
 
-        // on vérifie qu'il n'y a pas déjà quelqu'un sur ce créneau
+        // on vérifie s'il y a quelqu'un ou pas sur ce créneau
         if (planning[jour][heure].nom !== '') {
+          // on supprime le créneau
           planning[jour][heure].nom = '';
           planning[jour][heure].prenom = '';
           planning[jour][heure].chambre = '';
+
+          // on enregistre les modifications
           this.fs.writeFileSync('./plannings/' + residence + '/' + id + '.json', JSON.stringify(planning, null, 2));
 
+          // on ajoute la modification à l'historique
           this.addHistorique('Suppression du créneau', id, residence, nom, prenom, chambre, jour, heure);
 
+          // on renvoit une réponse positive à la suppression
           res.status(200).send('Suppression réussi');
         }
         else {
+          // on renvoi une erreur
           res.status(404).send('Il n\'y a pas d\'inscription sur ce créneau');
         }
       }
       catch (err) {
+        // on renvoi une erreur
         res.status(404).send('Erreur dans les informations fournies')
       }
     }
@@ -112,36 +147,48 @@ module.exports = class Gestion {
     }
   }
 
+  // remet à 0 le planning et l'historique qui lui est associé
   remiseZero(id, residence, res) {
+    // on récupère l'historique
     const planning = JSON.parse(fs.readFileSync('./plannings/' + residence + '/' + id + '.json'));
 
+    // on déplace les données de dimanche 2 dans dimanche 1
     planning.dimanche1 = JSON.parse(JSON.stringify(planning.dimanche2));
 
-    for (let jour in planning) {
-      if (jour !== 'dimanche1') {
+    // on remet le reste à 0
+    for (let jour in planning)
+      if (jour !== 'dimanche1')
         for (let heure in planning[jour])
           for (let data in planning[jour][heure])
             planning[jour][heure][data] = '';
-      }
-    }
 
+    // on enregistre les modifications
     fs.writeFileSync('./plannings/' + residence + '/' + id + '.json', JSON.stringify(planning, null, 2));
 
+    // on réinitialise l'historique
     this.initHistorique(id, residence);
 
+    // si la fonction a été appelé depuis une requète html, on renvoi une réponse positive
     if (res !== undefined) res.status(200).send('zero');
   }
 
+  // initialisation de l'historique
   initHistorique(id, residence) {
     const historique = {historique: []};
+
+    // on écrit la modification
     fs.writeFileSync('./historique/' + residence + '/' + 'historique_' + id + '.json', JSON.stringify(historique, null, 2));
   }
 
+  // ajout à l'historique
   addHistorique(modification, id, residence, nom, prenom, chambre, jour, heure) {
+    // on récupère la date actuelle
     const time = new Date(Date());
 
+    // on récupère l'historique
     const historique = JSON.parse(fs.readFileSync('./historique/' + residence + '/' + 'historique_' + id + '.json'));
 
+    // options pour l'enregistrement de la date
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' };
     historique.historique.push({
       modification: modification,
@@ -153,6 +200,7 @@ module.exports = class Gestion {
       heure: heure
     });
 
+    // on enregistre les modifications
     fs.writeFileSync('./historique/' + residence + '/' + 'historique_' + id + '.json', JSON.stringify(historique, null, 2));
   }
 }
