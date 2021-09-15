585d75feba9cab267e8521a04ee3a1624b8515d4
Nicolas Barrat <nicolas.barrat@student.junia.com>
2021-09-15 22:38:43 +0200

    Correction commandes + clean routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,6 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  // {
-  //   path: 'home',
-  //   loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  // },
-  // {
-  //   path: '',
-  //   redirectTo: 'home',
-  //   pathMatch: 'full'
-  // },
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
@@ -19,10 +10,6 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then( m => m.TabsPageModule)
   },
-  // {
-  //   path: 'liste',
-  //   loadChildren: () => import('./liste/liste.module').then( m => m.ListePageModule)
-  // },
 ];
 
 @NgModule({
