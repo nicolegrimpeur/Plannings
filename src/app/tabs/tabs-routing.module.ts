d06987e5b3e0fd6e2741af9ab87124f92147d288
Nicolas <nicolas.barrat@student.junia.com>
2022-08-13 20:41:07 +0200

    Correction tabs

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -5,13 +5,33 @@ import {TabsPage} from './tabs.page';
 
 const routes: Routes = [
   {
+    path: 'login',
+    loadChildren: () => import('../login/login.module').then(m => m.LoginPageModule)
+  },
+  {
+    path: 'infos-modal',
+    loadChildren: () => import('../shared/modal/infos-modal/infos-modal.module').then(m => m.InfosModalPageModule)
+  },
+  {
+    path: 'ajout-google-calendar',
+    loadChildren: () => import('../shared/modal/ajout-google-calendar/ajout-google-calendar.module').then(m => m.AjoutGoogleCalendarPageModule)
+  },
+  {
+    path: 'planning',
+    loadChildren: () => import('../planning/planning.module').then(m => m.PlanningPageModule)
+  },
+  {
+    path: 'historique',
+    loadChildren: () => import('../historique/historique.module').then(m => m.HistoriquePageModule)
+  },
+  {
+    path: 'erreur',
+    loadChildren: () => import('../erreur/erreur.module').then(m => m.ErreurPageModule)
+  },
+  {
     path: '',
     component: TabsPage,
     children: [
-      {
-        path: 'login',
-        loadChildren: () => import('../login/login.module').then(m => m.LoginPageModule)
-      },
       {
         path: 'home',
         loadChildren: () => import('../home/home.module').then(m => m.HomePageModule)
@@ -19,26 +39,6 @@ const routes: Routes = [
       {
         path: 'liste',
         loadChildren: () => import('../liste/liste.module').then(m => m.ListePageModule)
-      },
-      {
-        path: 'infos-modal',
-        loadChildren: () => import('../shared/modal/infos-modal/infos-modal.module').then(m => m.InfosModalPageModule)
-      },
-      {
-        path: 'ajout-google-calendar',
-        loadChildren: () => import('../shared/modal/ajout-google-calendar/ajout-google-calendar.module').then(m => m.AjoutGoogleCalendarPageModule)
-      },
-      {
-        path: 'planning',
-        loadChildren: () => import('../planning/planning.module').then(m => m.PlanningPageModule)
-      },
-      {
-        path: 'historique',
-        loadChildren: () => import('../historique/historique.module').then(m => m.HistoriquePageModule)
-      },
-      {
-        path: 'erreur',
-        loadChildren: () => import('../erreur/erreur.module').then(m => m.ErreurPageModule)
       },
       {
         path: '',
