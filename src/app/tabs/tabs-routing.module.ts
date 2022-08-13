e95650e7952395b866192531ae02aef64583283b
Nicolas <nicolas.barrat@student.junia.com>
2022-08-13 19:10:57 +0200

    Correction routage

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -1,25 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
-import { TabsPage } from './tabs.page';
+import {TabsPage} from './tabs.page';
 
 const routes: Routes = [
-  {
-    path: 'planning',
-    loadChildren: () => import('../planning/planning.module').then( m => m.PlanningPageModule)
-  },
-  {
-    path: 'historique',
-    loadChildren: () => import('../historique/historique.module').then( m => m.HistoriquePageModule)
-  },
-  {
-    path: 'erreur',
-    loadChildren: () => import('../erreur/erreur.module').then( m => m.ErreurPageModule)
-  },
   {
     path: '',
     component: TabsPage,
     children: [
+      {
+        path: 'login',
+        loadChildren: () => import('../login/login.module').then(m => m.LoginPageModule)
+      },
       {
         path: 'home',
         loadChildren: () => import('../home/home.module').then(m => m.HomePageModule)
@@ -27,6 +19,26 @@ const routes: Routes = [
       {
         path: 'liste',
         loadChildren: () => import('../liste/liste.module').then(m => m.ListePageModule)
+      },
+      {
+        path: 'infos-modal',
+        loadChildren: () => import('../shared/modal/infos-modal/infos-modal.module').then(m => m.InfosModalPageModule)
+      },
+      {
+        path: 'ajout-google-calendar',
+        loadChildren: () => import('../shared/modal/ajout-google-calendar/ajout-google-calendar.module').then(m => m.AjoutGoogleCalendarPageModule)
+      },
+      {
+        path: 'planning',
+        loadChildren: () => import('../planning/planning.module').then(m => m.PlanningPageModule)
+      },
+      {
+        path: 'historique',
+        loadChildren: () => import('../historique/historique.module').then(m => m.HistoriquePageModule)
+      },
+      {
+        path: 'erreur',
+        loadChildren: () => import('../erreur/erreur.module').then(m => m.ErreurPageModule)
       },
       {
         path: '',
@@ -41,4 +53,5 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class TabsPageRoutingModule {}
+export class TabsPageRoutingModule {
+}
