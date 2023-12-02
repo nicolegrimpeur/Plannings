9de734b19719b80c38c0769bd2835e4d05f3a303
Nicolas B <nicolegrimpeur@gmail.com>
2023-12-02 17:36:07 +0100

    Mise à jour des fichiers Android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,8 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'plannings.ionic.all',
   appName: 'All Plannings',
-  webDir: 'www',
-  bundledWebRuntime: false
+  webDir: 'www'
 };
 
 export default config;
